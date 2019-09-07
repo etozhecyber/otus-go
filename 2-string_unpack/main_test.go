309dfd3b39d32af88f796da24b99c06d6a5480fe
cyber <cyb@li.ru>
2019-09-07 17:14:11 +0300

    lesson-2: rework tests,use testcases

--- 2-string_unpack/main_test.go
+++ 2-string_unpack/main_test.go
@@ -6,9 +6,28 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type TestCaseType struct {
+	testData       string
+	expectedResult string
+}
+
 func TestUnpack(t *testing.T) {
-	require.Equal(t, unpack("a4bc2d5e"), "aaaabccddddde")
-	require.Equal(t, unpack("abcd"), "abcd")
-	require.Equal(t, unpack("45"), "")
-	require.Equal(t, unpack("a45"), "aaaa")
+
+	var testCases = []TestCaseType{
+		{testData: "a4bc2d5e2", expectedResult: "aaaabccdddddee"},
+		{testData: "abcd", expectedResult: "abcd"},
+		{testData: "45", expectedResult: ""},
+		{testData: "a123", expectedResult: "a"},
+		{testData: "a0123", expectedResult: ""},
+		{testData: "a0a", expectedResult: "a"},
+		{testData: "0a", expectedResult: "a"},
+		{testData: "0", expectedResult: ""},
+		{testData: "a", expectedResult: "a"},
+		{testData: "01a2c1", expectedResult: "aac"},
+	}
+
+	for _, testCase := range testCases {
+		require.Equal(t, unpack(testCase.testData), testCase.expectedResult)
+	}
+
 }
